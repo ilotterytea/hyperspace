0769ddedfcc0d875c076bd0e4256d38415b6c1d2
ilotterytea <ilotterytea@proton.me>
2023-05-18 15:38:15 +0600

    add: get instance

--- core/src/main/java/kz/ilotterytea/hyperspace/HyperspaceGame.java
+++ core/src/main/java/kz/ilotterytea/hyperspace/HyperspaceGame.java
@@ -4,6 +4,16 @@ import com.badlogic.gdx.Game;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class HyperspaceGame extends Game {
+    private static HyperspaceGame instance;
+
+    public static HyperspaceGame getInstance() {
+        return instance;
+    }
+
+    public HyperspaceGame() {
+        instance = this;
+    }
+
     @Override
     public void create() {
         setScreen(new FirstScreen());

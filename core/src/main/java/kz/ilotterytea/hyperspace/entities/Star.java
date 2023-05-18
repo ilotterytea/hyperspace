c47b1626d4f58fca93bb7cff0ac1b868ca2b31d8
ilotterytea <ilotterytea@proton.me>
2023-05-18 15:49:41 +0600

    add: star entity

---
+++ core/src/main/java/kz/ilotterytea/hyperspace/entities/Star.java
@@ -0,0 +1,32 @@
+package kz.ilotterytea.hyperspace.entities;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/**
+ * Star entity.
+ * @author ilotterytea
+ * @version 1.0
+ */
+public class Star extends Sprite {
+
+    /**
+     * Star entity.
+     * @author ilotterytea
+     */
+    public Star() {
+        super();
+    }
+
+    /**
+     * Update position.
+     */
+    public void update() {
+
+    }
+
+    @Override
+    public void draw(Batch batch) {
+        super.draw(batch);
+    }
+}

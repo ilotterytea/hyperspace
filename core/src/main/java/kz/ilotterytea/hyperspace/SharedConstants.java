25893633cdbd096acf60b8167f12314cd5589c63
ilotterytea <ilotterytea@proton.me>
2023-05-18 19:30:53 +0600

    add: max velocity

--- core/src/main/java/kz/ilotterytea/hyperspace/SharedConstants.java
+++ core/src/main/java/kz/ilotterytea/hyperspace/SharedConstants.java
@@ -8,8 +8,9 @@ import com.badlogic.gdx.graphics.Color;
  * @version 1.0
  */
 public class SharedConstants {
-    public static final int NUM_STARS = 100;
+    public static final int NUM_STARS = 2000;
     public static final int STARS_POS_Z_INIT_DISTANCE = 40;
 
     public static final Color[] STARS_COLORS = new Color[]{Color.CYAN, Color.SKY, Color.ROYAL, Color.BLUE};
+    public static final float STARS_MAX_VELOCITY = 0.25f;
 }

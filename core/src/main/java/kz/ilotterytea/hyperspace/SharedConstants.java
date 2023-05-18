63ae91446b017a2cd47ba18f7878e78c794748e0
ilotterytea <ilotterytea@proton.me>
2023-05-18 15:56:03 +0600

    add: shared constants

---
+++ core/src/main/java/kz/ilotterytea/hyperspace/SharedConstants.java
@@ -0,0 +1,10 @@
+package kz.ilotterytea.hyperspace;
+
+/**
+ * Shared constants.
+ * @author ilotterytea
+ * @version 1.0
+ */
+public class SharedConstants {
+    public static final int NUM_STARS = 100;
+}

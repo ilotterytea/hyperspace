4059069c8df6b513156473025339844be1329c44
ilotterytea <me@ilotterytea.kz>
2024-12-10 17:01:50 +0500

    feat: macroquad initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("hyperspace")]
+async fn main() {
+    loop {
+        clear_background(WHITE);
+
+        draw_text("hi world!", 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await
+    }
 }

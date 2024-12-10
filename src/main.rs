9e416df761bf8348c7410e4180375ae68129ead7
ilotterytea <me@ilotterytea.kz>
2024-12-10 17:33:53 +0500

    feat: render stars!

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,24 @@ mod star;
 async fn main() {
     let mut stars: Vec<Star> = Vec::new();
     for _ in 0..STAR_AMOUNT {
-        stars.push(Star {
-            position: Vec3::new(0.0, 0.0, 0.0),
-            size: Vec2::new(20.0, 20.0),
-            velocity: 1.0,
-            color: BLACK,
-        });
+        stars.push(Star::new());
     }
 
     loop {
         clear_background(WHITE);
 
-        for star in &stars {
+        for star in &mut stars {
+            star.position.z -= star.velocity;
+
+            if star.position.z < 1.0 {
+                star.position = generate_3d_position();
+            }
+
+            star.update();
+
             draw_rectangle(
-                star.position.x,
-                star.position.y,
+                star.render_position.x,
+                star.render_position.y,
                 star.size.x,
                 star.size.y,
                 star.color,
--- src/star.rs
+++ src/star.rs
@@ -1,12 +1,56 @@
+use std::f32::consts::PI;
+
+use ::rand::{thread_rng, Rng};
 use macroquad::prelude::*;
 
 pub const STAR_AMOUNT: i32 = 1000;
+pub const STAR_START_POS_Z: f32 = 20.0;
+pub const SPACE_SIZE: f32 = 32.0;
 
 pub struct Star {
     pub position: Vec3,
+    pub render_position: Vec2,
     pub size: Vec2,
     pub velocity: f32,
     pub color: Color,
 }
 
-impl Star {}
+impl Star {
+    pub fn new() -> Self {
+        Self {
+            position: generate_3d_position(),
+            render_position: Vec2::new(0.0, 0.0),
+            size: Vec2::new(1.0, 1.0),
+            velocity: 0.25,
+            color: BLACK,
+        }
+    }
+
+    pub fn update(&mut self) {
+        let screen_center_x = screen_width() / 2.0;
+        let screen_center_y = screen_height() / 2.0;
+
+        let x = self.position.x / self.position.z + screen_center_x;
+        let y = self.position.y / self.position.z + screen_center_y;
+
+        self.render_position.x = x;
+        self.render_position.y = y;
+
+        let size = (STAR_START_POS_Z - self.position.z) / (0.2 * self.position.z);
+
+        self.size.x = size;
+        self.size.y = size;
+    }
+}
+
+pub fn generate_3d_position() -> Vec3 {
+    let mut rand = thread_rng();
+    let angle = rand.gen::<f32>() * 2.0 * PI;
+    let radius =
+        (screen_height() / SPACE_SIZE) + (rand.gen::<f32>() * screen_height()) * SPACE_SIZE;
+
+    let x = radius * angle.sin();
+    let y = radius * angle.cos();
+
+    Vec3::new(x, y, STAR_START_POS_Z)
+}

b54f6defe7dc6ff51fe2ea64a25b511683cc06e4
ilotterytea <me@ilotterytea.kz>
2024-12-10 17:05:01 +0500

    feat: raylib initialization

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main(int argc, char *argv[]) {
-  printf("hi world!\n");
+  InitWindow(800, 600, "hyperspace (demo)");
+
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(WHITE);
+    DrawText("hi world!", GetScreenWidth() / 2.0 - 16 * 4,
+             GetScreenHeight() / 2.0 - 16, 32, BLACK);
+    EndDrawing();
+  }
+
   return 0;
 }
